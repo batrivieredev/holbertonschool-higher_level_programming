00c52d391e4ff883d8c45313e3ae1661839e1396
batrivieredev <batrivieredev@lenovo-ideapad1.>
2025-01-15 13:08:51 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(122, 96, -1):
-    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
+print("".join("zYxWvUtSrQpOnMlKjIhGfEdCbA"[i] for i in range(26)), end="")
