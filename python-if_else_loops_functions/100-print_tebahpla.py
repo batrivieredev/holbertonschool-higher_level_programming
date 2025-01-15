62457d25f90a30c07f184f0137cbf20ba85344ef
batrivieredev <batrivieredev@lenovo-ideapad1.>
2025-01-15 13:12:44 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join("zYxWvUtSrQpOnMlKjIhGfEdCbA"[i] for i in range(26)), end="")
+print("".join(chr(122 - i) if i % 2 == 0 else chr(90 - i) for i in range(26)), end="")
