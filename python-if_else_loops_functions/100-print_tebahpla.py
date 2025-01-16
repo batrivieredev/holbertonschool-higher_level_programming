cfb5ad298617ef92e0fe17627d73e8084ba868ed
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-16 14:54:55 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(c - 32) if c % 2 else chr(c) for c in range(122, 96, -1)), end="")
+print("".join(chr(c) if c % 2 == 0 else chr(c - 32) for c in range(122, 96, -1)), end="")
