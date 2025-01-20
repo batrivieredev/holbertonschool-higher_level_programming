1b3a3ef902b3c81ffb9e70cb45fae1240b75be95
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-20 19:05:26 +0100

    100-weight_average.py

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:  # If the list is empty, return 0
+        return 0
+
+    total_weighted_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_weighted_score += score * weight
+        total_weight += weight
+
+    return total_weighted_score / total_weight if total_weight != 0 else 0
