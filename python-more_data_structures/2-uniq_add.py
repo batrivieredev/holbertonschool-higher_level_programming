0abfa56a10a9365a7550ad1172e7dc930b72a41c
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-20 19:01:25 +0100

    2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+    #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    # Create a set to store unique values
+    unique_integers = set(my_list)
+    # Sum all the unique_integers)
+    return sum(unique_integers)
