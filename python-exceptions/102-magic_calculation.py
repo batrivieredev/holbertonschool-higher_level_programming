25da823952ad64504e16dc0c9ecb21e62378fad1
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-21 09:40:22 +0100

    102-magic_calculation.py

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,15 +2,12 @@
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):  # La boucle itère de 1 à 2 (inclus)
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception("Too far")
             result += (a ** b) / i
-        except Exception as e:
-            print(f"Exception caught: {e}")
-            result = 0  # Réinitialiser result si une exception est levée
-            break  # Sortir de la boucle si une exception est levée
-    if result == 0:  # Si la boucle a été interrompue par une exception
-        result = b + a
+        except Exception:
+            pass
+    result = b + a
     return result
