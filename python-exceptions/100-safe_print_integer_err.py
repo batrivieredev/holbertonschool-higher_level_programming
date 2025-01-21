de58c610d7bc1c823b490d5dd7d7372526235a76
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-21 09:35:40 +0100

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,11 @@
 
 def safe_print_integer_err(value):
     try:
+        # Try to print the value using the format function
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as e:
         # If a ValueError or TypeError occurs, print the exception to stderr
         import sys
-        print("Exception: ", e, file=sys.stderr)
+        print("Exception:", e, file=sys.stderr)
         return False
