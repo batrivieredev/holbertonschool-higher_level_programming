5d0b97d1cb89a70a9d0543b30b5df7aa40894260
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-22 10:28:32 +0100

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines a function `say_my_name` that prints a formatted string
-containing the first and last name provided as input.
+say_my_name function :
+Prints 'My name is <first name> <last name>'
+TypeError ifthe first_name or last_name is not a string
 """
 
 
-def say_my_name(first_name="", last_name=""):
-    """Function that prints 'My name is <first name> <last name>'
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print. Default is an empty string.
-
-    Raises:
-        TypeError: If either `first_name` or `last_name` is not a string.
+def say_my_name(first_name, last_name=""):
     """
-    if first_name is None and last_name is None:
-        raise TypeError("say_my_name() missing 'first_name' and 'last_name'")
-
-    if not isinstance(first_name, (str, type(None))):
+    Prints 'My name is <first name> <last name>'
+    """
+    if not type(first_name) is str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, (str, type(None))):
+    if not type(last_name) is str:
         raise TypeError("last_name must be a string")
-
-    if first_name and last_name:
-        print(f"My name is {first_name} {last_name}")
-    elif first_name:
-        print(f"My name is {first_name} ")
-    else:
-        print("No name provided")
+    print("My name is {} {}".format(first_name, last_name))
