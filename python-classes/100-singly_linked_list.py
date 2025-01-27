7a26d4cfe7dbf51d7af2f0893a7dc46705a71766
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-27 11:47:46 +0100

    100-singly_linked_list.py

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -5,52 +5,45 @@ class Node:
 
     @property
     def data(self):
-        return self._data
+        return self.__data
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self._data = value
+        self.__data = value
 
     @property
     def next_node(self):
-        return self._next_node
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
-        self._next_node = value
+        self.__next_node = value
 
 
 class SinglyLinkedList:
     def __init__(self):
-        self.head = None
+        self.__head = None
 
     def __str__(self):
-        # To print the list in a readable format
-        nodes = []
-        current = self.head
-        while current:
-            nodes.append(str(current.data))
+        current = self.__head
+        result = []
+        while current is not None:
+            result.append(str(current.data))
             current = current.next_node
-        return "\n".join(nodes)
+        return "\n".join(result)
 
     def sorted_insert(self, value):
-        # Create the new node
         new_node = Node(value)
-
-        # Case 1: If the list is empty or the new value should be at the head
-        if self.head is None or self.head.data >= value:
-            new_node.next_node = self.head
-            self.head = new_node
+        if self.__head is None or self.__head.data >= value:
+            new_node.next_node = self.__head
+            self.__head = new_node
         else:
-            # Case 2: Find the right position to insert
-            current = self.head
-            while current.next_node and current.next_node.data < value:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data < value:
                 current = current.next_node
-
-            # Insert the new node at the correct position
             new_node.next_node = current.next_node
             current.next_node = new_node
