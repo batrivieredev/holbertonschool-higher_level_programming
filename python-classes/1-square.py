6fa3872959ca9657ef03960cbc7f54758b7210d5
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-27 11:37:55 +0100

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains a class definition of 'Square'"""
+
+
+class Square():
+    """Definition of private size in Square"""
+    def __init__(self, x):
+        self.__size = x
