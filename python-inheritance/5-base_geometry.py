5ed8996ac32d422e91bb317e9a1d159702515f8d
batrivieredev <baptiste.rivierefr@gmail.com>
2025-02-03 10:05:58 +0100

    5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class.
+
+BaseGeometry serves as a base class for geometric shapes.
+It is intended to be subclassed by more specific geometric shape classes.
+"""
+
+
+class BaseGeometry:
+    """A base class for geometric shapes."""
+    pass
