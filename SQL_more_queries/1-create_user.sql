93531115904116030945a32b5e16592e5d2a266f
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-04 11:07:09 +0100

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- Script qui creer un fichier de configuration pour le serveur de base de donnees MySQL et un utilisateur user_0d_1 avec tous les privileges 
+-- Create l'utilisateur user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- lui ajouter tous les privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- verifier que les privileges sont bien pris en compte
+FLUSH PRIVILEGES;
