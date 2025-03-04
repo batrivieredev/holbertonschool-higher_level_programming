fade25f912048978c8858dd50ae9149e71389de1
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-04 11:09:41 +0100

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Script qui crée un utilisateur user_0d_2 avec tous les privileges sur la bdd
-CREATE USER 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- Script qui affiche les privilèges de l'utilisateur user_0d_1 et user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
