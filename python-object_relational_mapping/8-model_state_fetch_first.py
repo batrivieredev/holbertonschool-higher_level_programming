6ac1334172211cd3eba8ab48938cb26e1eb49713
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-04 13:28:45 +0100

    8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Imprime le premier objet State de la bdd hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user, passwd, database = argv[1], argv[2], argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
