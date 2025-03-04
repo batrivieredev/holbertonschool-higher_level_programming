aa1a783dc58e61c7e5c08c6e31075a3ae41d95fa
batrivieredev <baptiste.rivierefr@gmail.com>
2025-03-04 10:50:29 +0100

    16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
--- Script qui liste les noms et scores des élèves de la table second_table
+-- List toutes les tables de la bdd
+-- affiche le contenu de la table second_table
+SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
